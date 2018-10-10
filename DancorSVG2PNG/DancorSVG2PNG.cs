bc8c37a22ce1a808829eddb0c67a7c4d1673a318
osuhomebase <mbaron999@gmail.com>
2018-10-10 14:29:23 -0400

    added console app, working copy

--- DancorSVG2PNG/DancorSVG2PNG.cs
+++ DancorSVG2PNG/DancorSVG2PNG.cs
@@ -5,6 +5,9 @@ using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Host;
+using System.Diagnostics;
+using System;
+
 
 namespace DancorSVG2PNG
 {
@@ -27,9 +30,33 @@ namespace DancorSVG2PNG
                 name = data?.name;
             }
 
+            Process proc = new Process();
+            try
+            {
+                proc.StartInfo.UseShellExecute = false;
+                proc.StartInfo.CreateNoWindow = false;
+                proc.StartInfo.RedirectStandardOutput = true;
+                proc.StartInfo.FileName = "java.exe";
+                proc.StartInfo.Arguments = "-jar C:\\SourceCode\\DancorSVG2PNG-Function\\SVG2PNGConsole\\Batik\\batik-rasterizer.jar C:\\SourceCode\\DancorSVG2PNG-Function\\SVG2PNGConsole\\Test.svg";
+                proc.Start();
+                proc.WaitForExit();
+                if (proc.HasExited)
+                    log.Info(proc.StandardOutput.ReadToEnd());
+                log.Info("java.exe -jar C:\\SourceCode\\DancorSVG2PNG-Function\\SVG2PNGConsole\\Batik\\batik-rasterizer.jar C:\\SourceCode\\DancorSVG2PNG-Function\\SVG2PNGConsole\\Test.svg");
+                log.Info("success!");
+            }
+            catch (Exception e)
+            {
+                log.Info("Batik Fail");
+                log.Info(e.Message);
+            }
+
             return name == null
                 ? req.CreateResponse(HttpStatusCode.BadRequest, "Please pass a name on the query string or in the request body")
                 : req.CreateResponse(HttpStatusCode.OK, "Hello " + name);
+
+
+            
         }
     }
 }
---
+++ SVG2PNGConsole/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace SVG2PNGConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            Process proc = new Process();
+            try
+            {
+                proc.StartInfo.UseShellExecute = false;
+                proc.StartInfo.CreateNoWindow = false;
+                proc.StartInfo.RedirectStandardOutput = true;
+                proc.StartInfo.FileName = "java.exe";
+                proc.StartInfo.Arguments = "-jar C:\\SourceCode\\DancorSVG2PNG-Function\\SVG2PNGConsole\\Batik\\batik-rasterizer.jar C:\\SourceCode\\DancorSVG2PNG-Function\\SVG2PNGConsole\\Test.svg";
+                proc.Start();
+                proc.WaitForExit();
+                if (proc.HasExited)
+                    Console.WriteLine(proc.StandardOutput.ReadToEnd());
+                Console.WriteLine("java.exe -jar C:\\SourceCode\\DancorSVG2PNG-Function\\SVG2PNGConsole\\Batik\\batik-rasterizer.jar C:\\SourceCode\\DancorSVG2PNG-Function\\SVG2PNGConsole\\Test.svg");
+                Console.WriteLine("success!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Batik Fail");
+                Console.WriteLine(e.Message);
+            }
+            Console.WriteLine("Hello World!");
+            Console.ReadKey(true);
+        }
+    }
+}
